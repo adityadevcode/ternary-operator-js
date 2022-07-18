a4c2367ba22b8531d3107e7a95798bef671a86e7
addycoderepo <adityagitcode@gmail.com>
2022-07-18 13:16:43 -0400

    ternary operator

--- sample.js
+++ sample.js
@@ -1,2 +1,19 @@
-let name="some";
-console.log(name);
+/* we have binary operator and unary operator
+binary operator 2 inputs 2 values ni operation cheyadaniki use chesthundi
+uniary 1 input oka value isthe sari pothundi
+ternary 3 inputs
+*/
+
+/*
+Syntax:
+condition ? expression 1: expression 2;
+*/
+
+var big;
+var a=13;
+var b=8;
+/* we can write additional things
+big= a<b?a:b;
+*/
+//big =a<b?a*5:b*10;
+a>b ? document.write("a is big"):document.write("b is big ");
